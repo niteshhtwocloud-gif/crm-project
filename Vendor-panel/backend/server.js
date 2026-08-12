23dd67d9566784169f00f14e6b4d6a30af5683a9
niteshhtwocloud-gif <nitesh.htwocloud@gmail.com>
2026-08-12 18:22:04 +0530

    Update server.js

--- Vendor-panel/backend/server.js
+++ Vendor-panel/backend/server.js
@@ -4,7 +4,7 @@ require('dotenv').config({
 
 const express = require('express');
 const cors = require('cors');
-
+const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
